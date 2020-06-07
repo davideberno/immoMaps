1cdc497ae9f5813c14b7b2159dba9850c8eb7ae2
Davide Bernocchi <davide.berno84@gmail.com>
2020-06-07 09:57:29 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,139 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import {
+  GoogleMap,
+  LoadScript,
+  StandaloneSearchBox,
+  Marker,
+  InfoWindow,
+} from "@react-google-maps/api";
 
-function App() {
+import zones from "./zones";
+
+const libraries = ["places", "drawing", "geometry"];
+
+const App = () => {
+  const [center, setCenter] = useState({ lat: 41.902782, lng: 12.496365 });
+  const [zoom, setZoom] = useState(10);
+  const [marker, setMarker] = useState(null);
+  const [searchBox, setSearchBox] = useState(null);
+  const [infoWindow, setInfoWindow] = useState(null);
+  const [map, setMap] = useState(null);
+
+  useEffect(() => {
+    if (marker) {
+      setCenter(marker);
+      setZoom(12);
+    }
+  }, [marker]);
+
+  const searchBoxonLoad = (ref) => setSearchBox(ref);
+
+  const onPlacesChanged = () => {
+    console.log(searchBox.getPlaces());
+    const places = searchBox.getPlaces();
+    const newMarker = places[0].geometry.location;
+    setMarker(newMarker);
+  };
+
+  const checkPos = () => {
+    let result = null;
+    map.data.forEach((feature) => {
+      const geometry = feature.getGeometry();
+      if (geometry.getType() === "Polygon") {
+        const polygon = new window.google.maps.Polygon({
+          paths: geometry.getAt(0).getArray(),
+        });
+        if (
+          window.google.maps.geometry.poly.containsLocation(marker, polygon)
+        ) {
+          result = feature;
+        }
+      } else if (geometry.getType() === "GeometryCollection") {
+        const polygon = new window.google.maps.Polygon({
+          paths: geometry.getAt(0).getAt(0).getArray(),
+        });
+        if (
+          window.google.maps.geometry.poly.containsLocation(marker, polygon)
+        ) {
+          result = feature;
+        }
+      }
+    });
+    // if (result) {
+    //   map.data.overrideStyle(result, { visible: true });
+    // }
+    return result;
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <LoadScript
+      googleMapsApiKey="AIzaSyAx06IiRaZQ7CLnK26C1pU5uqg5z1SnxYo"
+      libraries={libraries}
+    >
+      <GoogleMap
+        mapContainerStyle={{
+          width: "100vw",
+          height: "100vh",
+        }}
+        center={center}
+        zoom={zoom}
+        onLoad={(map) => {
+          zones.forEach((zone) => map.data.addGeoJson(zone));
+          map.data.setStyle({ visible: false });
+          setMap(map);
+        }}
+      >
+        <StandaloneSearchBox
+          onLoad={searchBoxonLoad}
+          onPlacesChanged={onPlacesChanged}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <input
+            type="text"
+            placeholder="Search..."
+            style={{
+              border: `1px solid transparent`,
+              width: `340px`,
+              height: `32px`,
+              padding: `0 12px`,
+              borderRadius: `3px`,
+              boxShadow: `0 2px 6px rgba(0, 0, 0, 0.3)`,
+              fontSize: `14px`,
+              outline: `none`,
+              textOverflow: `ellipses`,
+              position: "absolute",
+              left: "50%",
+              marginLeft: "-120px",
+              marginTop: "10px",
+            }}
+          />
+        </StandaloneSearchBox>
+        {marker ? (
+          <Marker
+            position={marker}
+            visible={!infoWindow ? true : false}
+            onClick={(e) => setInfoWindow(marker)}
+          />
+        ) : null}
+        {infoWindow ? (
+          <InfoWindow
+            position={marker}
+            onLoad={(info) => console.log(checkPos())}
+            onCloseClick={() => setInfoWindow(null)}
+          >
+            <div
+              style={{
+                background: `white`,
+                border: `1px solid #ccc`,
+                padding: 15,
+              }}
+            >
+              <h1>{checkPos().getProperty("name")}</h1>
+            </div>
+          </InfoWindow>
+        ) : null}
+      </GoogleMap>
+    </LoadScript>
   );
-}
+};
 
 export default App;
---
+++ src/zones.js
@@ -0,0 +1,239 @@
+import z0 from "./zones/0.json";
+import z1 from "./zones/1.json";
+import z2 from "./zones/2.json";
+import z3 from "./zones/3.json";
+import z4 from "./zones/4.json";
+import z5 from "./zones/5.json";
+import z6 from "./zones/6.json";
+import z7 from "./zones/7.json";
+import z8 from "./zones/8.json";
+import z9 from "./zones/9.json";
+import z10 from "./zones/10.json";
+import z11 from "./zones/11.json";
+import z12 from "./zones/12.json";
+import z13 from "./zones/13.json";
+import z14 from "./zones/14.json";
+import z15 from "./zones/15.json";
+import z16 from "./zones/16.json";
+import z17 from "./zones/17.json";
+import z18 from "./zones/18.json";
+import z19 from "./zones/19.json";
+import z20 from "./zones/20.json";
+import z21 from "./zones/21.json";
+import z22 from "./zones/22.json";
+import z23 from "./zones/23.json";
+import z24 from "./zones/24.json";
+import z25 from "./zones/25.json";
+import z26 from "./zones/26.json";
+import z27 from "./zones/27.json";
+import z28 from "./zones/28.json";
+import z29 from "./zones/29.json";
+import z30 from "./zones/30.json";
+import z31 from "./zones/31.json";
+import z32 from "./zones/32.json";
+import z33 from "./zones/33.json";
+import z34 from "./zones/34.json";
+import z35 from "./zones/35.json";
+import z36 from "./zones/36.json";
+import z37 from "./zones/37.json";
+import z38 from "./zones/38.json";
+import z39 from "./zones/39.json";
+import z40 from "./zones/40.json";
+import z41 from "./zones/41.json";
+import z42 from "./zones/42.json";
+import z43 from "./zones/43.json";
+import z44 from "./zones/44.json";
+import z45 from "./zones/45.json";
+import z46 from "./zones/46.json";
+import z47 from "./zones/47.json";
+import z48 from "./zones/48.json";
+import z49 from "./zones/49.json";
+import z50 from "./zones/50.json";
+import z51 from "./zones/51.json";
+import z52 from "./zones/52.json";
+import z53 from "./zones/53.json";
+import z54 from "./zones/54.json";
+import z55 from "./zones/55.json";
+import z56 from "./zones/56.json";
+import z57 from "./zones/57.json";
+import z58 from "./zones/58.json";
+import z59 from "./zones/59.json";
+import z60 from "./zones/60.json";
+import z61 from "./zones/61.json";
+import z62 from "./zones/62.json";
+import z63 from "./zones/63.json";
+import z64 from "./zones/64.json";
+import z65 from "./zones/65.json";
+import z66 from "./zones/66.json";
+import z67 from "./zones/67.json";
+import z68 from "./zones/68.json";
+import z69 from "./zones/69.json";
+import z70 from "./zones/70.json";
+import z71 from "./zones/71.json";
+import z72 from "./zones/72.json";
+import z73 from "./zones/73.json";
+import z74 from "./zones/74.json";
+import z75 from "./zones/75.json";
+import z76 from "./zones/76.json";
+import z77 from "./zones/77.json";
+import z78 from "./zones/78.json";
+import z79 from "./zones/79.json";
+import z80 from "./zones/80.json";
+import z81 from "./zones/81.json";
+import z82 from "./zones/82.json";
+import z83 from "./zones/83.json";
+import z84 from "./zones/84.json";
+import z85 from "./zones/85.json";
+import z86 from "./zones/86.json";
+import z87 from "./zones/87.json";
+import z88 from "./zones/88.json";
+import z89 from "./zones/89.json";
+import z90 from "./zones/90.json";
+import z91 from "./zones/91.json";
+import z92 from "./zones/92.json";
+import z93 from "./zones/93.json";
+import z94 from "./zones/94.json";
+import z95 from "./zones/95.json";
+import z96 from "./zones/96.json";
+import z97 from "./zones/97.json";
+import z98 from "./zones/98.json";
+import z99 from "./zones/99.json";
+import z100 from "./zones/100.json";
+import z101 from "./zones/101.json";
+import z102 from "./zones/102.json";
+import z103 from "./zones/103.json";
+import z104 from "./zones/104.json";
+import z105 from "./zones/105.json";
+import z106 from "./zones/106.json";
+import z107 from "./zones/107.json";
+import z108 from "./zones/108.json";
+import z109 from "./zones/109.json";
+import z110 from "./zones/110.json";
+import z111 from "./zones/111.json";
+import z112 from "./zones/112.json";
+import z113 from "./zones/113.json";
+import z114 from "./zones/114.json";
+import z115 from "./zones/115.json";
+import z116 from "./zones/116.json";
+import z117 from "./zones/117.json";
+
+export default [
+  z0,
+  z1,
+  z2,
+  z3,
+  z4,
+  z5,
+  z6,
+  z7,
+  z8,
+  z9,
+  z10,
+  z11,
+  z12,
+  z13,
+  z14,
+  z15,
+  z16,
+  z17,
+  z18,
+  z19,
+  z20,
+  z21,
+  z22,
+  z23,
+  z24,
+  z25,
+  z26,
+  z27,
+  z28,
+  z29,
+  z30,
+  z31,
+  z32,
+  z33,
+  z34,
+  z35,
+  z36,
+  z37,
+  z38,
+  z39,
+  z40,
+  z41,
+  z42,
+  z43,
+  z44,
+  z45,
+  z46,
+  z47,
+  z48,
+  z49,
+  z50,
+  z51,
+  z52,
+  z53,
+  z54,
+  z55,
+  z56,
+  z57,
+  z58,
+  z59,
+  z60,
+  z61,
+  z62,
+  z63,
+  z64,
+  z65,
+  z66,
+  z67,
+  z68,
+  z69,
+  z70,
+  z71,
+  z72,
+  z73,
+  z74,
+  z75,
+  z76,
+  z77,
+  z78,
+  z79,
+  z80,
+  z81,
+  z82,
+  z83,
+  z84,
+  z85,
+  z86,
+  z87,
+  z88,
+  z89,
+  z90,
+  z91,
+  z92,
+  z93,
+  z94,
+  z95,
+  z96,
+  z97,
+  z98,
+  z99,
+  z100,
+  z101,
+  z102,
+  z103,
+  z104,
+  z105,
+  z106,
+  z107,
+  z108,
+  z109,
+  z110,
+  z111,
+  z112,
+  z113,
+  z114,
+  z115,
+  z116,
+  z117,
+];
